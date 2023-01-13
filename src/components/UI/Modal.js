ae5bbb6974c92054b82deebcceaa525f3220f719
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-14 01:56:20 +0530

    develop hide and show state of the cart modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from "./Modal.module.css"
 
 const Backdrop = (props) => {
     return(
-        <div className={classes.backdrop}></div>
+        <div className={classes.backdrop} onClick={props.onClick}></div>
     )
 }
 
@@ -22,7 +22,7 @@ const Modal = (props) => {
 
     return(
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClick={props.onClick}/>, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </Fragment>
     )
