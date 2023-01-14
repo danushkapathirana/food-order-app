113c7ef65c15eceb07a4533a9d3012ed176393da
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-14 15:35:57 +0530

    develop increase and decrease cart items from the cart modal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -30,6 +30,25 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmounnt
         }
     }
+    
+    if(action.type === "REMOVE_ITEM") {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id)
+        const existingCartItem = state.items[existingCartItemIndex]
+        const updatedTotalAmounnt = state.totalAmount - existingCartItem.price
+        let updatedItems
+
+        if(existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id)
+        } else {
+            const updatedItem =  {...existingCartItem, amount: existingCartItem.amount - 1}
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmounnt
+        }
+    }
     return defaultCartState
 }
 
