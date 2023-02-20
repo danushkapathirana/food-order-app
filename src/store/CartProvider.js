06ee906ea1bf620404d3553413aa807e096446df
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-20 09:35:42 +0530

    develop clearing cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -49,6 +49,11 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmounnt
         }
     }
+
+    if(action.type === "CLEAR") {
+        return defaultCartState
+    }
+
     return defaultCartState
 }
 
@@ -63,11 +68,16 @@ const CartProvider = (props) => {
         dispatchCartAction({type: "REMOVE_ITEM", id: id})
     }
 
+    const clearCartHandler = () => {
+        dispatchCartAction({type: "CLEAR"})
+    }
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler
     }
     return(
         <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>
