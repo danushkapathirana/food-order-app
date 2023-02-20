1e6cf49535586d98e1fe240ebcc2b74e94c154ac
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-20 08:05:00 +0530

    develop checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 import classes from "./Cart.module.css"
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false)
+
     const cartCtx = useContext(CartContext)
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -20,9 +23,20 @@ const Cart = (props) => {
         cartCtx.removeItem(id)
     }
 
+    const orderHandler = () => {
+        setIsCheckout(true)
+    }
+
     const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item) => (
     <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onAdd={cartItemAddHandler.bind(null, item)} onRemove={cartItemRemoveHandler.bind(null, item.id)} />
     ))}</ul>
+
+    const modalActions = (
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onCloseCart}>Close</button>
+            {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+        </div>
+    )
 
     return(
         <Modal onClick={props.onCloseCart}>
@@ -31,10 +45,8 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onCloseCart}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onCloseCart} />}
+            {!isCheckout && modalActions}
         </Modal>
     )
 }
