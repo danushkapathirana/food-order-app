cb172ddd205e8da57ef2387cf025e922e11f465b
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-20 07:02:28 +0530

    develop loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,6 +34,7 @@ import classes from "./AvailableMeals.module.css"
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     // useEffect should not return a promise
     // useEffect(async () => {await fetch("")}, []), not allowed
@@ -53,9 +54,18 @@ const AvailableMeals = () => {
                 })
             }
             setMeals(loadedMeals)
+            setIsLoading(false)
         }
         fetchMeals()
     }, [])
+
+    if(isLoading) {
+        return(
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        )
+    }
 
     const mealsList = meals.map((meal) => (<MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />))
 
